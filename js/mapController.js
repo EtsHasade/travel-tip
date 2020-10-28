9b9a8e109ea4d00e9d8f723ad04e9c7d52439ddd
Shani Saadia <shanisaadia@google.com>
2020-10-28 16:39:36 +0200

    Basic layout

--- js/mapController.js
+++ js/mapController.js
@@ -69,7 +69,7 @@ function getPosition() {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAygmMRDmE5l3JX0JxP9hmDtHdl5Tnddes';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
